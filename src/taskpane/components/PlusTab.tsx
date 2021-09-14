cadf68334dc9a030cd199c3b6ed866517fabccb0
Pierre-Alain Etique <paetique@hotmail.com>
2021-09-14 23:32:21 +0200

    EoD

--- src/taskpane/components/PlusTab.tsx
+++ src/taskpane/components/PlusTab.tsx
@@ -21,7 +21,7 @@
 import * as React from "react";
 import Config from "../Configs/Config";
 import CommandButton from "./CommandButton";
-import { Checkbox, DefaultButton, DefaultPalette, Dropdown, IButtonStyles, IDropdownOption, IDropdownStyles, IStackItemStyles, IStackTokens, ITextStyles, ResponsiveMode, Separator, Stack, Text } from "@fluentui/react";
+import { Checkbox, ChoiceGroup, ComboBox, DefaultButton, DefaultPalette, IButtonStyles, IChoiceGroupOption, IComboBox, IComboBoxOption, IStackItemStyles, IStackTokens, ITextStyles, Separator, Stack, Text } from "@fluentui/react";
 import LetterButton from "./LetterButton";
 
 export interface PlusTabProps {
@@ -48,6 +48,11 @@ const CommandStackTokens: IStackTokens = {
     padding: '2px 2px 2px 2px', // top right bottom left
 };
 
+const sylOptStackTokens: IStackTokens = { 
+    childrenGap: 12,
+    padding: '2px 2px 2px 2px', // top right bottom left
+};
+
 const flStackItemStyles: IStackItemStyles = {
     root: {
       alignItems: 'center',
@@ -57,6 +62,21 @@ const flStackItemStyles: IStackItemStyles = {
       overflow: 'hidden',
     },
 };
+
+// const cgStyles : IChoiceGroupOptionStyles = {
+//     choiceFieldWrapper: {width: 45, height: 45, borderColor: "#ff0000", border: "solid"},
+//     field: {
+//         // fontSize: 10,
+//         // color: "#00ff00"
+//     },
+//     iconWrapper: {maxWidth: 5},
+//     imageWrapper: {maxWidth: 5},
+//     innerField: {maxWidth: 5, color: "#00ff00"},
+//     input: {},
+//     labelWrapper: {maxWidth: 5},
+//     root: {},
+//     selectedImageWrapper: {maxWidth: 5},
+// }
 
 const customButStyles: IButtonStyles = { 
     root: {
@@ -77,24 +97,37 @@ const TextStyle: ITextStyles = {
 
 }
 
-const ddModeStyles: Partial<IDropdownStyles> = {
-    dropdown: { width: 80 },
-};
-
-const dd2ConsStyles: Partial<IDropdownStyles> = {
-    dropdown: { width: 100, fontSize: 12},
-};
-
-const sylModeOptions: IDropdownOption[] = [
+const sylModeOptions: IChoiceGroupOption[] = [
     { key: 'Ecrit', text: 'Écrit' },
     { key: 'Oral', text: 'Oral' },
     { key: 'Poésie', text: 'Poésie' },
 ];
 
-const cons2Options: IDropdownOption[] = [
+const cons2Options: IChoiceGroupOption[] = [
     { key: 'Std', text: 'Standard' },
     { key: 'Av2C', text: 'Avant 2 cons.' },
 ];
+
+const nrPiedsOptions: IComboBoxOption[] = [
+    { key: '0', text: 'Auto' },
+    { key: '1', text: '1' },
+    { key: '2', text: '2' },
+    { key: '3', text: '3' },
+    { key: '4', text: '4' },
+    { key: '5', text: '5' },
+    { key: '6', text: '6' },
+    { key: '7', text: '7' },
+    { key: '8', text: '8' },
+    { key: '9', text: '9' },
+    { key: '10', text: '10' },
+    { key: '11', text: '11' },
+    { key: '12', text: '12' },
+    { key: '13', text: '13' },
+    { key: '14', text: '14' },
+    { key: '15', text: '15' },
+    { key: '16', text: '16' },
+];
+
 
 export default function PlusTab(props: PlusTabProps) {
 
@@ -118,12 +151,25 @@ export default function PlusTab(props: PlusTabProps) {
         props.colNoir(props.conf);
     }
 
-    function OnSylModeChange(_ev: React.FormEvent<HTMLInputElement>, item: IDropdownOption): void  {
-        console.dir(item);
+    function OnSylModeChange(_ev: React.SyntheticEvent<HTMLElement>, option: IChoiceGroupOption): void  {
+        console.dir(option);
     }
 
-    function On2ConsChange(_ev: React.FormEvent<HTMLInputElement>, item: IDropdownOption): void  {
-        console.dir(item);
+    function On2ConsChange(_ev: React.SyntheticEvent<HTMLElement>, option: IChoiceGroupOption): void  {
+        console.dir(option);
+    }
+
+    function OnMuettesChange(_ev?: React.FormEvent<HTMLElement | HTMLInputElement>, isChecked?: boolean) {
+        console.log(`Muettes has been changed to ${isChecked}.`);
+    }
+
+    function OnDiereseChange(_ev?: React.FormEvent<HTMLElement | HTMLInputElement>, isChecked?: boolean) {
+        console.log(`Dierese has been changed to ${isChecked}.`);
+    }
+
+    function OnNbrePiedsChange(_ev: React.FormEvent<IComboBox>, _option?: IComboBoxOption, 
+        _index?: number, _value?: string){
+        console.log(`Nbre Pieds has been changed to index ${_index} and value ${_value}.`);
     }
 
     return (
@@ -202,45 +248,79 @@ export default function PlusTab(props: PlusTabProps) {
                 </Stack.Item>
             </Stack>
 
-            <Separator alignContent="start">{"Config Syllabes"}</Separator>
+            <Separator alignContent="center">{"Config Syllabes"}</Separator>
 
-            <Stack horizontal grow tokens={letterStackTokens}>
+            <Stack horizontal grow tokens={sylOptStackTokens}>
                 <Stack.Item>
-                    <Dropdown
-                        responsiveMode={ResponsiveMode.large}
+                    <ChoiceGroup
                         label="Mode"
                         defaultSelectedKey="Ecrit"
                         onChange={OnSylModeChange}
                         options={sylModeOptions}
-                        styles={ddModeStyles}
+                        styles={{
+                            root: { width: 80 },
+                            label: { paddingBottom: 0 },
+                        }}
                     />
                 </Stack.Item>
                 <Stack.Item>
-                    <Dropdown
-                        responsiveMode={ResponsiveMode.large}
-                        label="2 Consonnes"
-                        defaultSelectedKey="Std"
-                        onChange={On2ConsChange}
-                        options={cons2Options}
-                        styles={dd2ConsStyles}
-                    />
+                    <Stack>
+                        <Stack.Item>
+                            <ChoiceGroup
+                                label="2 Consonnes"
+                                defaultSelectedKey="Std"
+                                onChange={On2ConsChange}
+                                options={cons2Options}
+                                styles={{
+                                    root: { width: 115, },
+                                    label: { paddingBottom: 0 },
+                                }}
+                            />
+                        </Stack.Item>
+                        <Stack.Item>
+                            <Checkbox 
+                                label="Col Muettes" 
+                                styles={{
+                                    root: {paddingTop:'10px'},
+                                }}
+                                onChange={OnMuettesChange}
+                            />
+                        </Stack.Item>
+                    </Stack>
                 </Stack.Item>
                 <Stack.Item styles={{root: {paddingTop:'5px'}}}>
                     <Stack>
                         <Stack.Item>
                             <Text 
                                 variant='medium'
-                                styles={{root: { fontWeight: 600 }}}
+                                styles={{root: { fontWeight: "600" }}}
                             >
                                 Poésie
                             </Text>
                         </Stack.Item>
                         <Stack.Item>
-                            <Checkbox label="Diérèse" styles={{root: {paddingTop:'10px'}}}></Checkbox>
+                            <Checkbox 
+                                label="Diérèse" 
+                                disabled={false}
+                                styles={{root: {paddingTop:'10px'}}}
+                                onChange={OnDiereseChange}
+                            />
+                        </Stack.Item>
+                        <Stack.Item>
+                            <ComboBox
+                                defaultSelectedKey="0"
+                                label="Nbre pieds"
+                                options={nrPiedsOptions}
+                                styles={{ root: { width: 80 } }}
+                                disabled={false}
+                                onChange={OnNbrePiedsChange}
+                            />
                         </Stack.Item>
                     </Stack>
                 </Stack.Item>
-            </Stack>  
+            </Stack> 
+
+            <Separator alignContent="center">{"Couleurs alternées"}</Separator>
 
             
             
